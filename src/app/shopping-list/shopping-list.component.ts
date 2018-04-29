ac637fd233cb9a16b11b032488f34866862d90ca
Karthik <karthik19894@gmail.com>
2018-04-29 23:54:27 +0530

    Added Basic Templates to the Components and Model for Recipe

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients=[new Ingredient('Apples',2),new Ingredient('Tomatoes',5)];
   constructor() { }
 
   ngOnInit() {
